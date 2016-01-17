6a32239d2892340e31c59dd005907a158ca54a3e
Ryan Mitchell <ryan@Ryans-MacBook-Air.local>
2016-01-17 08:17:37 -0600

    Added drag and drop example

---
+++ 6-Selenium/2-dragAndDrop.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver import ActionChains
+
+#driver = webdriver.PhantomJS()
+driver = webdriver.Firefox()
+driver.get('http://pythonscraping.com/pages/javascript/draggableDemo.html')
+
+print(driver.find_element_by_id("message").text)
+
+element = driver.find_element_by_id("draggable")
+target = driver.find_element_by_id("div2")
+actions = ActionChains(driver)
+actions.drag_and_drop(element, target).perform()
+
+print(driver.find_element_by_id("message").text)
